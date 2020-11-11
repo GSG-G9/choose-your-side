40a55eaaaa65968c3c9d7b09d8cdc1d123ac7cdf
yasmeenattallah <yattallah7@gmail.com>
2020-11-11 14:17:01 +0200

    edit style

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const recipBtn = document.querySelector(".recipe-btn");
 const covidBtn = document.querySelector(".covid-btn");
 const mainRecipBtn = document.querySelector("#main-recipe");
 const mainCovidBtn = document.querySelector("#main-covid");
+const inputMeal = document.querySelector("#recipe-input-meal");
+const inputCountry = document.querySelector("#covid-input-country");
+const recipeSearchBtn = document.querySelector("#recipe-search-btn");
+const covidSearchBtn = document.querySelector("#covid-search-btn");
+const recipeResult = document.querySelector(".recipe-search-result");
+const covidResult = document.querySelector(".covid-search-btn");
 
 recipBtn.addEventListener('click' , ()=>{
  firstPage.style.display="none";
@@ -28,3 +34,24 @@ covidBtn.addEventListener('click' , ()=>{
     covidPage.style.display="none";
    
    })
+
+   function makeSearch (){
+    let xhr= XMLHttpRequest();
+    xhr.onreadystatechange = ()=>{
+        if(xhr.readyState == 4){
+            if(xhr.status == 200){
+               const resObj = JSON.parse(xhr.responseText);
+               console.log(resObj);
+               covidSearchBtn.addEventListener('click' , ()=>{
+               covidResult=resObj[0].Deaths;
+               })
+
+            }else {
+                console.log ("Error");
+            }
+        }
+    } 
+    const url = `https://api.covid19api.com/live/country/${inputCountry}`;
+  xhr.open("GET" , url ,true);
+  xhr.send();
+   }
